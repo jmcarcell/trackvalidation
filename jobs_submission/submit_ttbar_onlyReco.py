0d0c8de4f63b18f6a8b3d00b5b47f71cab0038ec
Erica Brondolin <ericabro@cern.ch>
2019-09-10 09:53:56 +0200

    First version of jub submission (muons and ttbar)

---
+++ jobs_submission/submit_ttbar_onlyReco.py
@@ -0,0 +1,55 @@
+import sys, os, subprocess
+from string import Template
+
+from DIRAC.Core.Base import Script
+Script.parseCommandLine()
+from ILCDIRAC.Interfaces.API.DiracILC import  DiracILC
+from ILCDIRAC.Interfaces.API.NewInterface.UserJob import *
+from ILCDIRAC.Interfaces.API.NewInterface.Applications import *
+
+sampleNum = 13837
+nameJobGroup = 'efficiencies'
+nameTag = 'ttbar'
+clicConfig = 'ILCSoft-2019-07-09'
+marlinVersion = 'ILCSoft-2019-07-09_gcc62'
+detectorModel =  'CLIC_o3_v14'
+nameDir = 'CLIC/2019/CLICo3v14/'+clicConfig+'/'+nameJobGroup+'/files_'+nameTag
+baseSteeringMarlin = '/home/ericabro/CLICstudies/2019/CPUtime_zcut/iLCSoft_2019-07-09/Reference/clicConfig/steeringFiles/clicReconstruction.xml'
+nameSteeringMarlin = "clicReconstruction_final.xml"
+templateOutRoot = "histograms"
+
+for input_ind in range(1,2,1):
+#for input_ind in range(1,4153,1):
+
+  rootFile = 'ttbar_' + str(input_ind)
+  with open(baseSteeringMarlin) as f:
+    open(nameSteeringMarlin,"w").write(f.read().replace(templateOutRoot,rootFile))
+
+  inputFolder_ind = input_ind / 1000
+  print ( inputFolder_ind )
+
+  dirac = DiracILC(False)
+  job = UserJob()
+  job.setJobGroup(nameJobGroup)
+  job.setName(nameTag)
+  job.setBannedSites(['LCG.IN2P3-CC.fr','OSG.UConn.us','LCG.Cracow.pl','OSG.MIT.us','LCG.Glasgow.uk','OSG.CIT.us','OSG.BNL.us','LCG.Brunel.uk','LCG.QMUL.uk'])
+  job.setOutputSandbox ( [ "*.log"] )
+  job.setOutputData([rootFile+".root"],nameDir,"CERN-DST-EOS")
+  
+  ma = Marlin()
+  ma.setVersion(marlinVersion)
+  #ma.setNbEvts(2)
+
+  ma.setInputFile("LFN:/ilc/prod/clic/3tev/tt/CLIC_o3_v14/SIM/000%s/00%s/tt_sim_%s_%s.slcio"%(str(sampleNum),str(inputFolder_ind),str(sampleNum),str(input_ind)))
+  ma.setDetectorModel(detectorModel)
+  ma.setSteeringFile(nameSteeringMarlin)
+  #ma.setNumberOfEvents(3)
+  
+  res=job.append(ma)
+  if not res['OK']:
+     print res['Message']
+     exit()
+
+  job.dontPromptMe()
+  print job.submit(dirac)
+      
