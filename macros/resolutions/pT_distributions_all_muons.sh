6f7e4836aa2bb1784314bfa3047bdbb04ce5d073
Emilia Leogrande <emilia.leogrande@cern.ch>
2020-02-21 11:10:04 +0000

    Upload New File

---
+++ macros/resolutions/pT_distributions_all_muons.sh
@@ -0,0 +1,81 @@
+source /cvmfs/clicdp.cern.ch/iLCSoft/builds/2019-02-20/x86_64-slc6-gcc62-opt/init_ilcsoft.sh
+eosfusebind
+#source pT_distributions.sh particleType energy angle momentum(no unit)
+source pT_distributions.sh muons 1GeV 10deg 1
+source pT_distributions.sh muons 1GeV 20deg 1
+source pT_distributions.sh muons 1GeV 30deg 1
+source pT_distributions.sh muons 1GeV 40deg 1
+source pT_distributions.sh muons 1GeV 50deg 1
+source pT_distributions.sh muons 1GeV 60deg 1
+source pT_distributions.sh muons 1GeV 70deg 1
+source pT_distributions.sh muons 1GeV 80deg 1
+source pT_distributions.sh muons 1GeV 89deg 1
+
+source pT_distributions.sh muons 2GeV 10deg 2
+source pT_distributions.sh muons 2GeV 30deg 2
+source pT_distributions.sh muons 2GeV 50deg 2
+source pT_distributions.sh muons 2GeV 70deg 2
+source pT_distributions.sh muons 2GeV 89deg 2
+
+source pT_distributions.sh muons 5GeV 10deg 5
+source pT_distributions.sh muons 5GeV 30deg 5
+source pT_distributions.sh muons 5GeV 50deg 5
+source pT_distributions.sh muons 5GeV 70deg 5
+source pT_distributions.sh muons 5GeV 89deg 5
+
+source pT_distributions.sh muons 10GeV 10deg 10
+source pT_distributions.sh muons 10GeV 20deg 10
+source pT_distributions.sh muons 10GeV 30deg 10
+source pT_distributions.sh muons 10GeV 40deg 10
+source pT_distributions.sh muons 10GeV 50deg 10
+source pT_distributions.sh muons 10GeV 60deg 10
+source pT_distributions.sh muons 10GeV 70deg 10
+source pT_distributions.sh muons 10GeV 80deg 10
+source pT_distributions.sh muons 10GeV 89deg 10
+
+source pT_distributions.sh muons 20GeV 10deg 20
+source pT_distributions.sh muons 20GeV 30deg 20
+source pT_distributions.sh muons 20GeV 50deg 20
+source pT_distributions.sh muons 20GeV 70deg 20
+source pT_distributions.sh muons 20GeV 89deg 20
+
+source pT_distributions.sh muons 50GeV 10deg 50
+source pT_distributions.sh muons 50GeV 30deg 50
+source pT_distributions.sh muons 50GeV 50deg 50
+source pT_distributions.sh muons 50GeV 70deg 50
+source pT_distributions.sh muons 50GeV 89deg 50
+
+source pT_distributions.sh muons 100GeV 10deg 100
+source pT_distributions.sh muons 100GeV 20deg 100
+source pT_distributions.sh muons 100GeV 30deg 100
+source pT_distributions.sh muons 100GeV 40deg 100
+source pT_distributions.sh muons 100GeV 50deg 100
+source pT_distributions.sh muons 100GeV 60deg 100
+source pT_distributions.sh muons 100GeV 70deg 100
+source pT_distributions.sh muons 100GeV 80deg 100
+source pT_distributions.sh muons 100GeV 89deg 100
+
+source pT_distributions.sh muons 200GeV 10deg 200
+source pT_distributions.sh muons 200GeV 30deg 200
+source pT_distributions.sh muons 200GeV 50deg 200
+source pT_distributions.sh muons 200GeV 70deg 200
+source pT_distributions.sh muons 200GeV 89deg 200
+
+source pT_distributions.sh muons 500GeV 10deg 500
+source pT_distributions.sh muons 500GeV 30deg 500
+source pT_distributions.sh muons 500GeV 50deg 500
+source pT_distributions.sh muons 500GeV 70deg 500
+source pT_distributions.sh muons 500GeV 89deg 500
+
+source pT_distributions.sh muons 1000GeV 10deg 1000
+source pT_distributions.sh muons 1000GeV 30deg 1000
+source pT_distributions.sh muons 1000GeV 50deg 1000
+source pT_distributions.sh muons 1000GeV 70deg 1000
+source pT_distributions.sh muons 1000GeV 89deg 1000
+
+source pT_distributions.sh muons 1500GeV 10deg 1500
+source pT_distributions.sh muons 1500GeV 30deg 1500
+source pT_distributions.sh muons 1500GeV 50deg 1500
+source pT_distributions.sh muons 1500GeV 70deg 1500
+source pT_distributions.sh muons 1500GeV 89deg 1500
+
