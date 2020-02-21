377a1e0b557d3b41f189044bf849af88ed34d89f
Emilia Leogrande <emilia.leogrande@cern.ch>
2020-02-21 11:08:08 +0000

    Upload New File

---
+++ macros/resolutions/cottheta_distributions_all_muons.sh
@@ -0,0 +1,81 @@
+source /cvmfs/clicdp.cern.ch/iLCSoft/builds/2019-02-20/x86_64-slc6-gcc62-opt/init_ilcsoft.sh
+eosfusebind
+#source cottheta_distributions.sh particleType energy angle momentum(no unit)
+source cottheta_distributions.sh muons 1GeV 10deg 
+source cottheta_distributions.sh muons 1GeV 20deg 
+source cottheta_distributions.sh muons 1GeV 30deg 
+source cottheta_distributions.sh muons 1GeV 40deg 
+source cottheta_distributions.sh muons 1GeV 50deg 
+source cottheta_distributions.sh muons 1GeV 60deg 
+source cottheta_distributions.sh muons 1GeV 70deg 
+source cottheta_distributions.sh muons 1GeV 80deg 
+source cottheta_distributions.sh muons 1GeV 89deg 
+
+source cottheta_distributions.sh muons 2GeV 10deg 
+source cottheta_distributions.sh muons 2GeV 30deg 
+source cottheta_distributions.sh muons 2GeV 50deg 
+source cottheta_distributions.sh muons 2GeV 70deg 
+source cottheta_distributions.sh muons 2GeV 89deg 
+
+source cottheta_distributions.sh muons 5GeV 10deg 
+source cottheta_distributions.sh muons 5GeV 30deg 
+source cottheta_distributions.sh muons 5GeV 50deg 
+source cottheta_distributions.sh muons 5GeV 70deg 
+source cottheta_distributions.sh muons 5GeV 89deg 
+
+source cottheta_distributions.sh muons 10GeV 10deg 
+source cottheta_distributions.sh muons 10GeV 20deg 
+source cottheta_distributions.sh muons 10GeV 30deg 
+source cottheta_distributions.sh muons 10GeV 40deg 
+source cottheta_distributions.sh muons 10GeV 50deg 
+source cottheta_distributions.sh muons 10GeV 60deg 
+source cottheta_distributions.sh muons 10GeV 70deg 
+source cottheta_distributions.sh muons 10GeV 80deg 
+source cottheta_distributions.sh muons 10GeV 89deg 
+
+source cottheta_distributions.sh muons 20GeV 10deg 
+source cottheta_distributions.sh muons 20GeV 30deg 
+source cottheta_distributions.sh muons 20GeV 50deg 
+source cottheta_distributions.sh muons 20GeV 70deg 
+source cottheta_distributions.sh muons 20GeV 89deg 
+
+source cottheta_distributions.sh muons 50GeV 10deg 
+source cottheta_distributions.sh muons 50GeV 30deg 
+source cottheta_distributions.sh muons 50GeV 50deg 
+source cottheta_distributions.sh muons 50GeV 70deg 
+source cottheta_distributions.sh muons 50GeV 89deg 
+
+source cottheta_distributions.sh muons 100GeV 10deg 
+source cottheta_distributions.sh muons 100GeV 20deg 
+source cottheta_distributions.sh muons 100GeV 30deg 
+source cottheta_distributions.sh muons 100GeV 40deg 
+source cottheta_distributions.sh muons 100GeV 50deg 
+source cottheta_distributions.sh muons 100GeV 60deg 
+source cottheta_distributions.sh muons 100GeV 70deg 
+source cottheta_distributions.sh muons 100GeV 80deg 
+source cottheta_distributions.sh muons 100GeV 89deg 
+
+source cottheta_distributions.sh muons 200GeV 10deg 
+source cottheta_distributions.sh muons 200GeV 30deg 
+source cottheta_distributions.sh muons 200GeV 50deg 
+source cottheta_distributions.sh muons 200GeV 70deg 
+source cottheta_distributions.sh muons 200GeV 89deg 
+
+source cottheta_distributions.sh muons 500GeV 10deg 
+source cottheta_distributions.sh muons 500GeV 30deg 
+source cottheta_distributions.sh muons 500GeV 50deg 
+source cottheta_distributions.sh muons 500GeV 70deg 
+source cottheta_distributions.sh muons 500GeV 89deg 
+
+source cottheta_distributions.sh muons 1000GeV 10deg 
+source cottheta_distributions.sh muons 1000GeV 30deg 
+source cottheta_distributions.sh muons 1000GeV 50deg 
+source cottheta_distributions.sh muons 1000GeV 70deg 
+source cottheta_distributions.sh muons 1000GeV 89deg 
+
+source cottheta_distributions.sh muons 1500GeV 10deg 
+source cottheta_distributions.sh muons 1500GeV 30deg 
+source cottheta_distributions.sh muons 1500GeV 50deg 
+source cottheta_distributions.sh muons 1500GeV 70deg 
+source cottheta_distributions.sh muons 1500GeV 89deg 
+
