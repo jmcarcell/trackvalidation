8fc253d2720cad9a00cc559de063062e84f34bc3
Emilia Leogrande <emilia.leogrande@cern.ch>
2020-02-21 11:00:30 +0000

    Upload New File

---
+++ jobs_submission/resolutions/run_lcio_particle_gun_11_1GeV_10deg.sh
@@ -0,0 +1,4 @@
+source /cvmfs/clicdp.cern.ch/iLCSoft/builds/nightly/x86_64-slc6-gcc7-opt/init_ilcsoft.sh  
+
+#python lcio_particle_gun.py nEvts pt pdg charge theta
+python lcio_particle_gun.py 1000 1 11 -1 10
