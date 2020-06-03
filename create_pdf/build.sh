52bcb7ea842904a8859170b05f8e185adb6b85a2
Erica Brondolin <ericabro@cern.ch>
2020-06-03 18:42:05 +0200

    Add pdf with comparison of two releases

--- create_pdf/build.sh
+++ create_pdf/build.sh
@@ -1,9 +1,15 @@
 PATHPLOTS="/home/ericabro/CLICstudies/2020/trackingvalidation_finalise/plots/clic_releases_validation/ILCSoft-2019-09-04/"
 NEWFOLDER="plots/"
+NEWFOLDER2="plots/"
 
 #Copying files in the folder
 sh copy_plots.sh $PATHPLOTS $NEWFOLDER
 
-python personalise_pdf.py --authorinstitute CERN --authorname "Erica Brondolin" --date 03-06-2020 --release "ILCSoft-2020-02-07" --path $NEWFOLDER --minHits_singlePart "3" --minHits_complex "4"
+python personalise_pdf.py --filein "release_summary_template.tex" --fileout "release_summary.tex" --authorinstitute CERN --authorname "Erica Brondolin" --date 03-06-2020 --release "ILCSoft-2020-02-07" --path $NEWFOLDER --minHits_singlePart "3" --minHits_complex "4"
 pdflatex release_summary.tex
+
+python personalise_pdf.py --filein "release_comparison_template.tex" --fileout "release_comparison.tex" --authorinstitute CERN --authorname "Erica Brondolin" --date 03-06-2020 --release "ILCSoft-2019-09-04" --path $NEWFOLDER --minHits_singlePart "3" --minHits_complex "4" --release2 "ILCSoft-2020-02-07" --path2 $NEWFOLDER2
+pdflatex release_comparison.tex
+
 evince release_summary.pdf
+evince release_comparison.pdf
--- create_pdf/personalise_pdf.py
+++ create_pdf/personalise_pdf.py
@@ -3,22 +3,26 @@ import fileinput
 import argparse
 
 parser = argparse.ArgumentParser(description='Produce pdf summary file starting from release_summary_template.tex')
+parser.add_argument('--filein', help='Name file input')
+parser.add_argument('--fileout', help='Name file output')
 parser.add_argument('--release', help='Release for which the validation was performed')
+parser.add_argument('--path', help='Path for plots folder')
 parser.add_argument('--authorname', help='Author name')
 parser.add_argument('--authorinstitute', help='Author institute')
 parser.add_argument('--date', help='Date of the presentation')
-parser.add_argument('--path', help='Path for plots folder')
 parser.add_argument('--minHits_singlePart', help='Minim number of hits in single particles')
 parser.add_argument('--minHits_complex', help='Minim number of hits in complex events')
+parser.add_argument('--release2', help='Comparison release for which the validation was also performed')
+parser.add_argument('--path2', help='Path for plots in comparison folder')
 args = parser.parse_args()
 print(args)
 
-NAMEINFILE  = "release_summary_template.tex"
-NAMEOUTFILE = "release_summary.tex"
-REPLACEARGS = {"RELEASE": args.release, "AUTHORNAME": args.authorname,
-               "AUTHORINSTITUTE": args.authorinstitute, "DATE": args.date,
+NAMEINFILE  = args.filein#"release_summary_template.tex"
+NAMEOUTFILE = args.fileout#"release_summary.tex"
+REPLACEARGS = {"RELEASE": args.release, "PATHPLOTS": args.path, "DATE": args.date, 
+               "AUTHORNAME": args.authorname, "AUTHORINSTITUTE": args.authorinstitute,
                "MINHITS_SINGLE": args.minHits_singlePart, "MINHITS_COMPLEX": args.minHits_complex,
-               "PATHPLOTS": args.path}
+               "REL2": args.release2, "PATH2": args.path2}
 
 #Open file
 f = open(NAMEINFILE,'r')
