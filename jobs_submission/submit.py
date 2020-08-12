65dd6df4552f7ae1170f9b13384f7010379a10d7
Erica Brondolin <ericabro@cern.ch>
2020-08-12 13:43:24 +0200

    Add ttbar submit

--- jobs_submission/submit.py
+++ jobs_submission/submit.py
@@ -2,11 +2,20 @@ import sys
 import os
 import json
 
-def main(argv):
-  if len(argv) == 0:
-    print("Please always give the run_config file as argument, e.g. \n python main.py ../cfg/run_info_local.json")
-    sys.exit()
+def check_file_exist(file_name):
+  isthere = os.path.isfile(file_name)
+  if not isthere :
+    print("> Input xml file (%s) does NOT exist."%(file_name))
+    check_xml = raw_input("> Do you want to copy the xml locally? [yes/no] ") 
+    if str(check_xml) == 'yes':
+      cmd_cp = "cp -r $ILCSOFT/ClicPerformance/HEAD/clicConfig/clic* local_files/"
+      os.system(cmd_cp)
+      print(cmd_cp)
+      print(">> Adapt the files to run only tracking or only validation! Example in local_files/templates/")
+      sys.exit()
+  return isthere
 
+def main(argv):
   run_config_file = argv[0]
   with open(run_config_file) as json_run_file:
     run_conf = json.load(json_run_file)
@@ -15,51 +24,94 @@ def main(argv):
   print(run_conf["Particles"])
   print(run_conf["Variables"])
   print(run_conf["Jobs_customised_lib"])
+  print(run_conf["Test Mode"])
+  print(run_conf["NEvents per job"])
+  print(run_conf["NJobs"])
+  print(run_conf["Type sample ttbar"])
+
+  #store variables from json
+  nev_per_job = run_conf["NEvents per job"]
+  n_jobs = run_conf["NJobs"]
+  eos_output_folder = run_conf["Jobs_output_folder"]
+  eos_custom_lib = run_conf["Jobs_customised_lib"]
+  test = run_conf["Test Mode"]
+
+  #adapt to test mode
+  if bool(test) :
+    print("> Running in test mode, only 2 events are submit with 3 events")
+    nev_per_job = 3
+    n_jobs = 2
 
   #set up environment
   cmd_source_ILCSoft = "source_ILCSoft %s"%(run_conf["Release date"])
   os.system(cmd_source_ILCSoft)
-  print(cmd_source_ILCSoft)
+  if bool(test) : print("Source ILCSoft release with: %s"%cmd_source_ILCSoft)
 
-  check_xml = raw_input("Do you want to copy the xml locally? [yes/no] ") 
-  if str(check_xml) == 'yes':
-    cmd_cp = "cp -r $ILCSOFT/ClicPerformance/HEAD/clicConfig/clic* local_files/"
-    os.system(cmd_cp)
-    print(cmd_cp)
-    print(">> Adapt the files to run only tracking! Example in local_files/clicReconstruction_template.xml")
-    sys.exit()
+  single_particle_list = ["muons", "electrons", "pions"]
+  complex_events_list = ["ttbar"]
 
-  particle_list = ["muons", "electrons", "pions"]
-  eos_output_folder = run_conf["Jobs_output_folder"]
-  eos_custom_lib = run_conf["Jobs_customised_lib"]
-  if not eos_custom_lib :
-    print("here")
-    eos_custom_lib = " "
-
+  #running sim-reco-val for single particles
   for particle in run_conf["Particles"]:
-    print(particle)
-    if particle not in particle_list:
-      print("%s not in the list of particles! Possible choices are: %s"%(particle,', '.join(particle_list)))
-      sys.exit()
+    if particle not in single_particle_list:
+      print("%s not in the list of single particles! Possible choices are: %s"%(particle,', '.join(single_particle_list)))
+      continue
+
+    #all sumbit scripts for single particle use the same xml file
+    xml_file_name = "local_files/clicReconstruction.xml"
+    if check_file_exist(xml_file_name) and bool(test):
+      print("> Input xml file already exist")
+
     for var in run_conf["Variables"]:
-      print(var)
       if var == "theta":
         for theta in ["10","30","89"] :
-          print(">> Running simulation and reconstruction for %s on grid at theta = %s deg"%(particle, theta))
+          if bool(test) : print(">> Running simulation and reconstruction for %s on grid at theta = %s deg"%(particle, theta))
           cmd_run_theta = "python submit_gps_theta.py %s %s ILCSoft-%s %s %s %s %s %s"%(particle, theta, run_conf["Release date"], run_conf["Detector model"], 
-                          run_conf["NJobs"], run_conf["NEvents_per_job"], eos_output_folder, "" if not eos_custom_lib else eos_custom_lib)
+                          n_jobs, nev_per_job, eos_output_folder, "" if not eos_custom_lib else eos_custom_lib)
           print(cmd_run_theta)
           #os.system(cmd_run_theta)
       elif var == "energy":
         for energy in ["1","10","100"] :
-          print(">> Running simulation and reconstruction for %s on grid at energy = %s GeV"%(particle, energy))
+          if bool(test) : print(">> Running simulation and reconstruction for %s on grid at energy = %s GeV"%(particle, energy))
           cmd_run_energy = "python submit_gun_energy.py %s %s ILCSoft-%s %s %s %s %s %s"%(particle, energy, run_conf["Release date"], run_conf["Detector model"], 
-                           run_conf["NJobs"], run_conf["NEvents_per_job"], eos_output_folder, "" if not eos_custom_lib else eos_custom_lib)
+                           n_jobs, nev_per_job, eos_output_folder, "" if not eos_custom_lib else eos_custom_lib)
           print(cmd_run_energy)
           #os.system(cmd_run_energy)
 
+  #running reco-val for complex events
+  for particle in run_conf["Particles"]:
+    if particle not in complex_events_list:
+      print("%s not in the list of complex events! Possible choices are: %s"%(particle,', '.join(complex_events_list)))
+      continue
+
+    if run_conf["Type sample ttbar"] == "SIM":
+      #check (or create) the input xml file
+      xml_file_name = "local_files/clicReconstruction.xml"
+      if check_file_exist(xml_file_name) and bool(test):
+        print("> Input xml file already exist")
+
+      if bool(test) : print(">> Running reconstruction and validation for %s on grid"%(particle))
+      cmd_run_ttbar = "python submit_ttbar_fromSIM.py %s %s ILCSoft-%s %s %s %s %s %s"%(particle, run_conf["Input sample ttbar"], run_conf["Release date"], run_conf["Detector model"], 
+                      n_jobs, nev_per_job, eos_output_folder, "" if not eos_custom_lib else eos_custom_lib)
+      print(cmd_run_ttbar)
+      #os.system(cmd_run_ttbar)
+
+    if run_conf["Type sample ttbar"] == "REC":
+      #check (or create) the input xml file
+      xml_file_name = "local_files/clicReconstruction_onlyValidator.xml"
+      if check_file_exist(xml_file_name) and bool(test):
+        print("> Input xml file already exist")
+
+      if bool(test) : print(">> Running validation for %s on grid"%(particle))
+      cmd_run_ttbar = "python submit_ttbar_fromREC.py %s %s ILCSoft-%s %s %s %s %s %s"%(particle, run_conf["Input sample ttbar"], run_conf["Release date"], run_conf["Detector model"], 
+                      n_jobs, nev_per_job, eos_output_folder, "" if not eos_custom_lib else eos_custom_lib)
+      print(cmd_run_ttbar)
+      #os.system(cmd_run_ttbar)
 
 if __name__ == "__main__":
+  if len(sys.argv[1:]) == 0:
+    print("Please always give the run_config file as argument, e.g. \n python main.py ../cfg/run_info_local.json")
+    sys.exit()
+
   check_dirac = raw_input("Did you setup Dirac? [yes/no] ") 
   if str(check_dirac) == 'yes':
     main(sys.argv[1:])
--- jobs_submission/submit_ttbar_fromSIM.py
+++ jobs_submission/submit_ttbar_fromSIM.py
@@ -7,21 +7,25 @@ from ILCDIRAC.Interfaces.API.DiracILC import  DiracILC
 from ILCDIRAC.Interfaces.API.NewInterface.UserJob import *
 from ILCDIRAC.Interfaces.API.NewInterface.Applications import *
 
-sampleNum = 13837
-nameJobGroup = 'efficiencies'
-nameTag = 'ttbar'
-clicConfig = 'ILCSoft-2019-07-09'
-marlinVersion = 'ILCSoft-2019-07-09_gcc62'
-detectorModel =  'CLIC_o3_v14'
-nameDir = 'CLIC/2019/CLICo3v14/'+clicConfig+'/'+nameJobGroup+'/files_'+nameTag
-baseSteeringMarlin = '/home/ericabro/CLICstudies/2019/CPUtime_zcut/iLCSoft_2019-07-09/Reference/clicConfig/steeringFiles/clicReconstruction.xml'
-nameSteeringMarlin = "clicReconstruction_final.xml"
+particle = sys.argv[1]
+sampleNum = sys.argv[2]
+nameTag = particle
+
+clicConfig = sys.argv[3]
+marlinVersion = sys.argv[3]+'_gcc62'
+detectorModel =  sys.argv[4]
+baseSteeringMarlin = 'local_files/clicReconstruction.xml'
+nameSteeringMarlin = 'local_files/clicReconstruction_final.xml'
+
+nJobs = int(sys.argv[5])
+nEvts = int(sys.argv[6])
+nameJobGroup = sys.argv[7]
 templateOutRoot = "histograms"
+nameDir = 'CLIC/'+detectorModel+'/'+clicConfig+'/'+nameJobGroup+'/files_'+nameTag
 
-for input_ind in range(1,2,1):
-#for input_ind in range(1,4153,1):
+for input_ind in range(1,nJobs,1):
 
-  rootFile = 'ttbar_' + str(input_ind)
+  rootFile = nameTag+'_' + str(input_ind)
   with open(baseSteeringMarlin) as f:
     open(nameSteeringMarlin,"w").write(f.read().replace(templateOutRoot,rootFile))
 
@@ -35,15 +39,20 @@ for input_ind in range(1,2,1):
   job.setBannedSites(['LCG.IN2P3-CC.fr','OSG.UConn.us','LCG.Cracow.pl','OSG.MIT.us','LCG.Glasgow.uk','OSG.CIT.us','OSG.BNL.us','LCG.Brunel.uk','LCG.QMUL.uk'])
   job.setOutputSandbox ( [ "*.log"] )
   job.setOutputData([rootFile+".root"],nameDir,"CERN-DST-EOS")
+  #set customised library
+  if len(sys.argv) > 8:
+    customisedLibrary = str(sys.argv[8])
+    print('Using Marlin customised library: %s'%customisedLibrary)
+    job.setInputSandbox(customisedLibrary)
+
   
   ma = Marlin()
   ma.setVersion(marlinVersion)
-  #ma.setNbEvts(2)
 
   ma.setInputFile("LFN:/ilc/prod/clic/3tev/tt/CLIC_o3_v14/SIM/000%s/00%s/tt_sim_%s_%s.slcio"%(str(sampleNum),str(inputFolder_ind),str(sampleNum),str(input_ind)))
   ma.setDetectorModel(detectorModel)
   ma.setSteeringFile(nameSteeringMarlin)
-  #ma.setNumberOfEvents(3)
+  ma.setNumberOfEvents(nEvts)
   
   res=job.append(ma)
   if not res['OK']:
