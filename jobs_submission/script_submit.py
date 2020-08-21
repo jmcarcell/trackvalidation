44a8c35b4e72ee855d57c0bff6b7daa2111d0ef7
Erica Brondolin <ericabro@cern.ch>
2020-08-21 09:40:35 +0200

    Update script_submit

--- jobs_submission/script_submit.py
+++ jobs_submission/script_submit.py
@@ -28,11 +28,6 @@ def main(argv):
     nev_per_job = 3
     n_jobs = 2
 
-  #set up environment
-  cmd_source_ILCSoft = "source_ILCSoft %s"%(run_conf["Release date"])
-  os.system(cmd_source_ILCSoft)
-  if bool(test) : print("Source ILCSoft release with: %s"%cmd_source_ILCSoft)
-
   single_particle_list = ["muons", "electrons", "pions"]
   complex_events_list = ["ttbar", "ttbar_ove"]
 
@@ -51,15 +46,15 @@ def main(argv):
       if var == "theta":
         for theta in ["10","30","89"] :
           if bool(test) : print(">> Running simulation and reconstruction for %s on grid at theta = %s deg"%(particle, theta))
-          cmd_run_theta = "python submit_allSingleParticles_gps_theta.py %s %s ILCSoft-%s %s %s %s %s %s"%(particle, theta, run_conf["Release date"], run_conf["Detector model"], 
-                          n_jobs, nev_per_job, eos_output_folder, "" if not eos_custom_lib else eos_custom_lib)
+          cmd_run_theta = "python submit_allSingleParticles_gps_theta.py -p %s -v %s -r ILCSoft-%s -D %s -j %s -e %s -g %s %s"%(particle, theta, run_conf["Release date"], run_conf["Detector model"], 
+                          n_jobs, nev_per_job, eos_output_folder, "" if not eos_custom_lib else (" -l "+eos_custom_lib))
           print(cmd_run_theta)
           os.system(cmd_run_theta)
       elif var == "energy":
         for energy in ["1","10","100"] :
           if bool(test) : print(">> Running simulation and reconstruction for %s on grid at energy = %s GeV"%(particle, energy))
-          cmd_run_energy = "python submit_allSingleParticles_gun_energy.py %s %s ILCSoft-%s %s %s %s %s %s"%(particle, energy, run_conf["Release date"], run_conf["Detector model"], 
-                           n_jobs, nev_per_job, eos_output_folder, "" if not eos_custom_lib else eos_custom_lib)
+          cmd_run_energy = "python submit_allSingleParticles_gun_energy.py -p %s -v %s -r ILCSoft-%s -D %s -j %s -e %s -g %s %s"%(particle, energy, run_conf["Release date"], run_conf["Detector model"], 
+                           n_jobs, nev_per_job, eos_output_folder, "" if not eos_custom_lib else (" -l "+eos_custom_lib))
           print(cmd_run_energy)
           os.system(cmd_run_energy)
       elif var == "pT" or var == "pt":
@@ -68,7 +63,7 @@ def main(argv):
           if str(step) == 'sim':
             if bool(test) : print(">> Producing sim files for %s on grid at pt = %s GeV"%(particle, pt))
             #ERICA: in this step 10'000 events are always created!
-            cmd_run_pt = "python submit_allSingleParticles_slcio_pt.py %s %s ILCSoft-%s %s %s %s %s "%(particle, pt, run_conf["Release date"], run_conf["Detector model"], 
+            cmd_run_pt = "python submit_allSingleParticles_sim_pt.py -p %s -v %s -r ILCSoft-%s -D %s -j %s -e %s -g %s"%(particle, pt, run_conf["Release date"], run_conf["Detector model"], 
                              n_jobs, nev_per_job, eos_output_folder)
             print(cmd_run_pt)
             os.system(cmd_run_pt)
@@ -83,7 +78,7 @@ def main(argv):
             #Sample fixed pt SIM folder is used to set the path to input SIM files
             #In this step n_jobs is the number of input SIM files taken from "Sample fixed pt SIM folder"
             #1000 events are always run (10'000 / 1'000 = 10 jobs per file)!
-            cmd_run_pt = "python submit_allSingleParticles_ddsim_pt.py %s %s ILCSoft-%s %s %s %s %s %s"%(particle, pt, run_conf["Release date"], run_conf["Detector model"], 
+            cmd_run_pt = "python submit_allSingleParticles_ddsim_pt.py -p %s -v %s -r ILCSoft-%s -D %s -j %s -e %s -g %s -f %s"%(particle, pt, run_conf["Release date"], run_conf["Detector model"], 
                              n_jobs, "1000", eos_output_folder, run_conf["Sample fixed pt SIM folder"])
             print(cmd_run_pt)
             os.system(cmd_run_pt)
@@ -99,8 +94,8 @@ def main(argv):
             #Sample fixed pt SIM folder is used to set the path to input DDSIM files
             #In this step n_jobs is the number of input SIM files taken from "Sample fixed pt SIM folder"
             #1000 events are always run (10'000 / 1'000 = 10 jobs per file)!
-            cmd_run_pt = "python submit_allSingleParticles_reco_pt.py %s %s ILCSoft-%s %s %s %s %s %s %s"%(particle, pt, run_conf["Release date"], run_conf["Detector model"], 
-                             n_jobs, "1000", eos_output_folder, run_conf["Sample fixed pt SIM folder"], "" if not eos_custom_lib else eos_custom_lib)
+            cmd_run_pt = "python submit_allSingleParticles_reco_pt.py -p %s -v %s -r ILCSoft-%s -D %s -j %s -e %s -g %s -f %s %s"%(particle, pt, run_conf["Release date"], run_conf["Detector model"], 
+                             n_jobs, "1000", eos_output_folder, run_conf["Sample fixed pt SIM folder"], "" if not eos_custom_lib else (" -l "+eos_custom_lib))
             print(cmd_run_pt)
             os.system(cmd_run_pt)
           else:
@@ -121,8 +116,8 @@ def main(argv):
         print("> Input xml file already exist")
 
       if bool(test) : print(">> Running reconstruction and validation for %s on grid"%(particle))
-      cmd_run_ttbar = "python submit_ttbar_fromSIM.py %s %s ILCSoft-%s %s %s %s %s %s"%(particle, run_conf["Input sample ttbar"], run_conf["Release date"], run_conf["Detector model"], 
-                      n_jobs, nev_per_job, eos_output_folder, "" if not eos_custom_lib else eos_custom_lib)
+      cmd_run_ttbar = "python submit_ttbar_fromSIM.py -p %s -f %s -r ILCSoft-%s -D %s -j %s -e %s -g %s %s"%(particle, run_conf["Input sample ttbar"], run_conf["Release date"], run_conf["Detector model"], 
+                      n_jobs, nev_per_job, eos_output_folder, "" if not eos_custom_lib else (" -l "+eos_custom_lib))
       print(cmd_run_ttbar)
       os.system(cmd_run_ttbar)
 
@@ -133,8 +128,8 @@ def main(argv):
         print("> Input xml file already exist")
 
       if bool(test) : print(">> Running validation for %s on grid"%(particle))
-      cmd_run_ttbar = "python submit_ttbar_fromREC.py %s %s ILCSoft-%s %s %s %s %s %s"%(particle, run_conf["Input sample ttbar"], run_conf["Release date"], run_conf["Detector model"], 
-                      n_jobs, nev_per_job, eos_output_folder, "" if not eos_custom_lib else eos_custom_lib)
+      cmd_run_ttbar = "python submit_ttbar_fromREC.py -p %s -f %s -r ILCSoft-%s -D %s -j %s -e %s -g %s %s"%(particle, run_conf["Input sample ttbar"], run_conf["Release date"], run_conf["Detector model"], 
+                      n_jobs, nev_per_job, eos_output_folder, "" if not eos_custom_lib else (" -l "+eos_custom_lib))
       print(cmd_run_ttbar)
       os.system(cmd_run_ttbar)
 
